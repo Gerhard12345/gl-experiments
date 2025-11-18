da11b096125d85e9c5fc1aa0dff1dc2308104e31
Gerhard Kitzler <gerhard.kitzler@gmx.at>
2025-11-18 16:45:58 +0100

    Added pytest settings and a unit test

---
+++ tests/unit_tests/test_material.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from customgl.objects.material import Material
+
+class TestMaterial:
+    def setup_method(self):
+        print("Setup")
+    
+    def teardown_method(self):
+        print("Teardown")
+
+    def test_constructor(self):
+        specularpower="str"
+        texture_scales=[2,4,3]
+        texturefilename=Path("texturefilename") 
+        texturefilename_normals=Path("texturefilename_normals")
+        texturefilename_ambient_occlusion=Path("texturefilename_ambient_occlusion")
+        texturefilename_specular=Path("texturefilename_specular")
+        material = Material(specularpower=specularpower,
+                                 texture_scales=texture_scales,
+                                 texturefilename=texturefilename,
+                                 texturefilename_normals=texturefilename_normals,
+                                 texturefilename_ambient_occlusion=texturefilename_ambient_occlusion,
+                                 texturefilename_specular=texturefilename_specular)
+        assert material.specularpower == specularpower
+        assert material.texture_scales == texture_scales
+        assert material.texturefilename == texturefilename
+        assert material.texturefilename_normals == texturefilename_normals
+        assert material.texturefilename_ambient_occlusion == texturefilename_ambient_occlusion
+        assert material.texturefilename_specular == texturefilename_specular
