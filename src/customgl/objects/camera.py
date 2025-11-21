Fix surface wall length computation and divide result by 2*pi.

src/customgl/objects/camera.py
@@ -10,8 +10,8 @@ class Camera:
         self.eye = np.array(eye, dtype=np.float64)
         self.up = np.array(up, dtype=np.float64)
         at = np.array(at)
-        self.d = np.linalg.norm(eye - at)
-        direction = 1 / self.d * (eye - at)
+        d = np.linalg.norm(eye - at)
+        direction = 1 / d * (eye - at)
         self.phi = np.arctan2(direction[2], direction[0])
         self.theta = np.arccos(direction[1])
         self.near = near
@@ -48,14 +48,16 @@ class Camera:
 
     def rotate_theta(self, dtheta):
         self.theta += np.deg2rad(dtheta)
-        self.eye = self.at + self.d * np.array(
+        d = np.linalg.norm(self.eye - self.at)
+        self.eye = self.at + d * np.array(
             [np.cos(self.phi) * np.sin(self.theta), np.cos(self.theta), np.sin(self.phi) * np.sin(self.theta)]
         )
         self.lookAt()
 
     def rotate_phi(self, dphi):
         self.phi += np.deg2rad(dphi)
-        self.eye = self.at + self.d * np.array(
+        d = np.linalg.norm(self.eye - self.at)
+        self.eye = self.at + d * np.array(
             [
                 np.cos(self.phi) * np.sin(self.theta),
                 np.cos(self.theta),
@@ -67,7 +69,7 @@ class Camera:
     def zoom(self, zoom_factor: float):
         direction = (self.at - self.eye) * zoom_factor
         self.eye = self.at - direction
-        self.d = np.linalg.norm(self.eye - self.at)
+        #self.d = np.linalg.norm(self.eye - self.at)
         self.lookAt()
 
     def translate(self, translation: List[float]):

src/customgl/objects/surface.py
@@ -222,7 +222,7 @@ class MeshedSurfaceWall(Object3d):
             for v, v_next in zip(self.meshed_surface.v_values, self.meshed_surface.v_values[1:]):
                 fullnodes = []
                 d = [self.meshed_surface.analytical_domain.Jf(u0, v)[1, 0], self.meshed_surface.analytical_domain.Jf(u0, v)[1, 1]]
-                sums[1] = sums[0] + np.linalg.norm(d)
+                sums[1] = sums[0] + np.linalg.norm(d)*(v_next-v)/(2*np.pi)
                 for is_bottom in [0, 1]:
                     for number, v_local in enumerate([v, v_next]):
                         x, y, z0 = self.meshed_surface.value(u0, v_local)